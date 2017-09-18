4916c6437af94c8ed7cbcf8782575e3bd144b8bd
Tughi <admin@tughi.com>
2017-09-18 21:24:30 +0200

    Add initial status call

--- translate/urls.py
+++ translate/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^(?P<translator_uuid>[0-9a-f-]+)/projects/(?P<project_uuid>[0-9a-f-]+)/languages/(?P<language_code>[a-z]{2}(_[A-Z]{2})?)/status$',
+        views.status,
+        name='status'),
 ]
--- translate/views.py
+++ translate/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from .models import Language
+from .models import Project
 
 
-def index(request):
-    return HttpResponse('Hello world')
+def status(request, translator_uuid, project_uuid, language_code):
+    try:
+        project = Project.objects.get(uuid=project_uuid)
+    except Project.DoesNotExist:
+        return JsonResponse(dict(message="Project does not exist"), status=404)
+
+    try:
+        language = project.languages.get(code=language_code)
+    except Language.DoesNotExist:
+        try:
+            language = project.languages.get(code=language_code.split('_')[0])
+        except Language.DoesNotExist:
+            return JsonResponse(dict(message="Language does not exist"), status=404)
+
+    return JsonResponse(dict(
+        language_code=language.code,
+        strings=project.strings.count(),
+    ))
