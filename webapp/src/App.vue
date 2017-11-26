91aff4b343a0e58c86cc709ccfc51d410153048e
Tughi <admin@tughi.com>
2017-11-26 08:20:04 +0100

    Add Vue.js project structure

---
+++ webapp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
