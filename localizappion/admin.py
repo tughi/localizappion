74063f12f54b8f1c8f36563c087509ca4d7372e1
Tughi <admin@tughi.com>
2018-01-18 20:31:25 +0100

    Add Knockout-based skeleton

--- localizappion/admin.py
+++ localizappion/admin.py
@@ -7,5 +7,5 @@ blueprint = flask.Blueprint(__name__.split('.')[-1], __name__)
 @blueprint.route('/admin')
 def project_list():
     return render_template(
-        'admin/app.html',
+        'admin/index.html',
     )
---
+++ localizappion/static/admin/router.js
@@ -0,0 +1,31 @@
+define(['knockout', 'crossroads', 'hasher'], function (ko, crossroads, hasher) {
+    'use strict';
+
+    function Router(config) {
+        this.currentRoute = ko.observable({});
+
+        ko.utils.arrayForEach(config.routes, route => {
+            crossroads.addRoute(route.url, params => {
+                this.currentRoute(ko.utils.extend(params, route.params));
+            });
+        });
+    };
+
+    var router = new Router({
+        routes: [
+            { url: 'projects', params: { page: 'projects' } },
+            { url: 'projects/{projectId}', params: { page: 'project-detail' } },
+            { url: 'projects/{projectId}/screenshots', params: { page: 'project-screenshots' } },
+            { url: 'projects/{projectId}/screenshots/{screenshotId}', params: { page: 'project-screenshot' } },
+            { url: 'projects/{projectId}/strings', params: { page: 'project-strings' } },
+            { url: 'projects/{projectId}/translations', params: { page: 'project-translations' } },
+        ]
+    });
+
+    crossroads.normalizeFn = crossroads.NORM_AS_OBJECT;
+    hasher.initialized.add(hash => crossroads.parse(hash));
+    hasher.changed.add(hash => crossroads.parse(hash));
+    hasher.init();
+
+    return router;
+});
