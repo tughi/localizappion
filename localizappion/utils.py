567ebff1f0c2b8ce6d0376d37e83d77789b773cc
Tughi <admin@tughi.com>
2018-01-28 13:55:02 +0100

    Add safety check

--- localizappion/utils.py
+++ localizappion/utils.py
@@ -5,5 +5,7 @@ import flask
 
 
 def create_hash(text):
+    if text is None:
+        return None
     hash_data = '{0}+{1}'.format(text, flask.current_app.secret_key)
     return base64.standard_b64encode(sha512(hash_data.encode()).digest()).decode()
