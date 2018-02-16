e635419f491d9b962e136ad85e26115f1b19b180
Tughi <admin@tughi.com>
2018-02-16 23:51:06 +0100

    Upgrade bootstrap version

--- localizappion/static/admin/main.js
+++ localizappion/static/admin/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: 'static/admin',
     paths: {
-        'bootstrap': 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.3/js/bootstrap.min',
+        'bootstrap': 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min',
         'crossroads': 'https://cdnjs.cloudflare.com/ajax/libs/crossroads/0.12.2/crossroads.min',
         'hasher': 'https://cdnjs.cloudflare.com/ajax/libs/hasher/1.2.0/hasher.min',
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
